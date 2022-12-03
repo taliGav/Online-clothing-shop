d2206e79b897030ccfd0528007dc49c5b337b585
Tali <taligavrielov@gmail.com>
2022-12-03 21:24:27 +0200

    seperate components

--- src/App.js
+++ src/App.js
@@ -1,47 +1,37 @@
+import Categories from "./components/Categories/Categories";
+
 function App() {
+  const categories = [
+    {
+      id: 1,
+      title: "hats",
+      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+    },
+    {
+      id: 2,
+      title: "jackets",
+      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+    },
+    {
+      id: 3,
+      title: "sneakers",
+      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+    },
+    {
+      id: 4,
+      title: "womens",
+      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+    },
+    {
+      id: 5,
+      title: "mens",
+      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+    },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>My React App</h1>
-      </header>
-
-      <div className="main-container">
-        <div className="category-container">
-          {/* <img/> */}
-          <div className="category-body-container">
-            <h2>Hats</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
-        <div className="category-container">
-          {/* <img/> */}
-          <div className="category-body-container">
-            <h2>Jackets</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
-        <div className="category-container">
-          {/* <img/> */}
-          <div className="category-body-container">
-            <h2>Sneakers</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
-        <div className="category-container">
-          {/* <img/> */}
-          <div className="category-body-container">
-            <h2>Women</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
-        <div className="category-container">
-          {/* <img/> */}
-          <div className="category-body-container">
-            <h2>Men</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
-      </div>
+      <Categories categories={categories} />
     </div>
   );
 }
---
+++ src/components/Categories/Categories.js
@@ -0,0 +1,20 @@
+import CategoryItem from "./../CategoryItem/CategoryItem";
+import "./categories.styles.scss";
+
+function Categories({categories}) {
+
+  return (
+    <div className="categories">
+      <h2>Categories</h2>
+      <div className="categories__container">
+        {categories.map((category) => (
+          <CategoryItem 
+		  key={category.id}
+		  category={category} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Categories;
