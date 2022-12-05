abb215abbbf5fbc153f937a619fa82544ce235c6
Tali <taligavrielov@gmail.com>
2022-12-05 15:06:43 +0200

    add Navigation.js to routes folder

---
+++ src/routes/Navigation/Navigation.js
@@ -0,0 +1,24 @@
+import { Fragment } from "react";
+import { Outlet , Link} from "react-router-dom";
+import "./navigation.styles.scss";
+import { ReactComponent as CrwnLogo } from "./../../assets/crown.svg";
+
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className="navigation-bar">
+        <Link className="logo-container" to="/">
+           <CrwnLogo className="logo" />
+        </Link>
+        <div className="nav-links-container">
+            <Link className="nav-link" to="/">Home</Link>
+            <Link className="nav-link" to="/shop">Shop</Link>
+            <Link className="nav-link" to="/contact">Contact</Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
+
+export default Navigation;
