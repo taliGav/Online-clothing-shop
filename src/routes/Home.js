0fa6e07f02dd341941e85f7ec383c9f7f2d46350
Tali <taligavrielov@gmail.com>
2022-12-05 15:06:33 +0200

    add Home.js to routes folder

---
+++ src/routes/Home.js
@@ -0,0 +1,39 @@
+import Categories from "../components/Categories/Categories";
+
+function Home() {
+  const categories = [
+    {
+      id: 1,
+      title: "HATS",
+      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+    },
+    {
+      id: 2,
+      title: "JACKETS",
+      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+    },
+    {
+      id: 3,
+      title: "SNEAKERS",
+      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+    },
+    {
+      id: 4,
+      title: "WOMEN",
+      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+    },
+    {
+      id: 5,
+      title: "MEN",
+      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+    },
+  ];
+
+  return (
+    <div className="Home">
+      <Categories categories={categories} />
+    </div>
+  );
+}
+
+export default Home;
