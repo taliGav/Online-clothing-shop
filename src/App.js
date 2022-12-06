1ea05b9a7515b71b3f95fafe0eaa56b28589580f
Tali <taligavrielov@gmail.com>
2022-12-06 11:02:23 +0200

    update routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Navigation from "./routes/navigation/Navigation";
-import SignIn from "./routes/sign-in/SignIn";
+import Authentication from "./routes/authentication/Authentication";
 import Home from "./routes/Home";
 
 const Shop = () => {
@@ -15,7 +15,7 @@ const App = () => {
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
-        <Route path="sign-in" element={<SignIn />} />
+        <Route path="auth" element={<Authentication />} />
       </Route>
     </Routes>
   );
