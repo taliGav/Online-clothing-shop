e0fb7c1e70744d6af458b1bfced4da704844bc8b
Tali <taligavrielov@gmail.com>
2022-12-06 14:22:36 +0200

    add context and sign out

--- src/routes/Navigation/Navigation.js
+++ src/routes/Navigation/Navigation.js
@@ -1,19 +1,38 @@
-import { Fragment } from "react";
-import { Outlet , Link} from "react-router-dom";
+import { Fragment, useContext } from "react";
+import { Outlet, Link } from "react-router-dom";
+
 import { ReactComponent as CrwnLogo } from "./../../assets/crown.svg";
+import { userContext } from "./../../contexts/user-context";
+
+import {signOutAuthUser} from "./../../utils/firebase/firebase-utils";
+
 import "./navigation.styles.scss";
 
 const Navigation = () => {
+  const { currentUser, setCurrentUser } = useContext(userContext);
+  // console.log("currentUser: ", currentUser);
+
+  const signOutHandler = async () => {
+    await signOutAuthUser();
+    setCurrentUser(null)
+  };
+
   return (
     <Fragment>
       <div className="navigation-bar">
         <Link className="logo-container" to="/">
-           <CrwnLogo className="logo" />
+          <CrwnLogo className="logo" />
         </Link>
         <div className="nav-links-container">
-            <Link className="nav-link" to="/shop">Shop</Link>
-            {/* <Link className="nav-link" to="/contact">Contact</Link> */}
-            <Link className="nav-link" to="/auth">SIGN IN</Link>
+          <Link className="nav-link" to="/shop">
+            Shop
+          </Link>
+          {/* <Link className="nav-link" to="/contact">Contact</Link> */}
+          {currentUser ? (<span className="nav-link" onClick={signOutHandler}>SIGN OUT</span>) : (
+          <Link className="nav-link" to="/auth">
+            SIGN IN
+          </Link>
+          )}
         </div>
       </div>
       <Outlet />
