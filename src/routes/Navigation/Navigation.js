e996b209a337afd525ac5b87e33745105b4a0339
Tali <taligavrielov@gmail.com>
2022-12-06 11:41:24 +0200

    update imports

--- src/routes/Navigation/Navigation.js
+++ src/routes/Navigation/Navigation.js
@@ -1,7 +1,7 @@
 import { Fragment } from "react";
 import { Outlet , Link} from "react-router-dom";
-import "./navigation.styles.scss";
 import { ReactComponent as CrwnLogo } from "./../../assets/crown.svg";
+import "./navigation.styles.scss";
 
 const Navigation = () => {
   return (
