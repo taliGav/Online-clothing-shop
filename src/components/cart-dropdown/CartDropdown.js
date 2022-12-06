3561595145c7d100eea4f2aa7bae334a58da9804
Tali <taligavrielov@gmail.com>
2022-12-06 19:28:37 +0200

    add cart dropdows component

---
+++ src/components/cart-dropdown/CartDropdown.js
@@ -0,0 +1,18 @@
+import "./cart-dropdown.styles.scss";
+
+import Button from "./../button/Button";
+
+const CartDropdown = () => {
+
+    return (
+    <div className="cart-dropdown-container">
+
+      <div className="cart-items">
+        
+      </div>
+        <Button>GO TO CHECKOUT</Button>
+    </div>
+  );
+};
+
+export default CartDropdown;
