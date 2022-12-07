2304db4aa2861fd7c35905507572d052a0701fcc
Tali <taligavrielov@gmail.com>
2022-12-07 12:55:01 +0200

    add itemsInCart to cart Icon

--- src/components/cart-icon/CartIcon.js
+++ src/components/cart-icon/CartIcon.js
@@ -1,13 +1,15 @@
 import { ReactComponent as ShoppingIcon } from "./../../assets/shopping-bag.svg";
-
+import { useContext } from "react";
+import { cartContext } from "./../../contexts/cart-context";
 import "./cart-icon.styles.scss";
 
 const CartIcon = ({onClick}) => {
-  const number = 10;
+  const {itemsInCart} = useContext(cartContext);
+
   return (
     <div onClick={onClick} className="cart-icon-container">
       <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">{number}</span>
+      <span className="item-count">{itemsInCart}</span>
     </div>
   );
 };
