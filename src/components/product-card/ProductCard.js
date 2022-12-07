78e9b57fb2da89afd5f3338d4d2fab0c4f69160f
Tali <taligavrielov@gmail.com>
2022-12-07 12:56:30 +0200

    add addItemToCart func to productCard component

--- src/components/product-card/ProductCard.js
+++ src/components/product-card/ProductCard.js
@@ -1,9 +1,14 @@
 import "./product-card.styles.scss";
-
+import { useContext } from "react";
+import { cartContext } from "./../../contexts/cart-context";
 import Button from "./../button/Button";
 
 const ProductCard = ({ product }) => {
   const { name, price, imageUrl } = product;
+  const { addItemToCart } = useContext(cartContext);
+
+  const handleAddToCart = () => addItemToCart(product)
+
   return (
     <div className="product-card-container">
       {/* <div className="product-card-image"> */}
@@ -16,7 +21,7 @@ const ProductCard = ({ product }) => {
         <span className="name">{name}</span>
         <span className="price">${price}</span>
       </div>
-      <Button>Add to cart</Button>
+      <Button onClick={handleAddToCart}>Add to cart</Button>
     </div>
   );
 };
