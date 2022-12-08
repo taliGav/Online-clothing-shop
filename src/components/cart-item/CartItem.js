0bfc8a3e5a98b76de9f1aae16195f264dc93a79c
Tali <taligavrielov@gmail.com>
2022-12-09 00:22:32 +0200

    update dropdown cart item

--- src/components/cart-item/CartItem.js
+++ src/components/cart-item/CartItem.js
@@ -1,13 +1,16 @@
 import "./cart-item.styles.scss";
 
-const CartItem = ({cartItem}) => {
-  const { name,
-    //  price, imageUrl,
-      quantity } = cartItem;
+const CartItem = ({ cartItem }) => {
+  const { name, price, imageUrl, quantity } = cartItem;
   return (
     <div className="cart-item-container">
-      <h2 className="name">{name}</h2>
-      <span className="quantity">{quantity}</span>
+      <img src={imageUrl || "https://via.placeholder.com/150"} alt={name} />
+      <div className="item-details">
+        <h2 className="name">{name}</h2>
+        <span className="quantity">
+          {quantity} X ${price}
+        </span>
+      </div>
     </div>
   );
 };
