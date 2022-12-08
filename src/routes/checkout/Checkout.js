7c2411d58f6ef365185126f8c0f262066af6750e
Tali <taligavrielov@gmail.com>
2022-12-09 00:21:42 +0200

    add checkout cmp

---
+++ src/routes/checkout/Checkout.js
@@ -0,0 +1,61 @@
+import { useContext } from "react";
+import { cartContext } from "./../../contexts/cart-context";
+import "./checkout.styles.scss";
+import CheckoutItem from "./../../components/checkout-item/CheckoutItem";
+// import { createAuthUserWithEmailAndPassword } from "../../utils/firebase/firebase-utils";
+
+const Checkout = () => {
+  const { cartItems, totalPrice } = useContext(cartContext);
+
+  //   const {
+  //     cart,
+  //     totalUniqueItems,
+  //     cartSubtotal,
+  //     cartTax,
+  //     cartTotal,
+  //     clearCart,
+  //     removeItem,
+  //     toggleAmount,
+  //   } = useCartContext();
+  //   const history = useHistory();
+
+  //   const isEmpty = !cart.length;
+  //   const isEmptyCart = isEmpty && totalUniqueItems === 0;
+
+  //   const handleSubmit = (e) => {
+  //     e.preventDefault();
+  //     clearCart();
+  //     history.push("/");
+  //   };
+
+  return (
+    <div className="checkout-container">
+      <div className="checkout-header">
+        <div className="header-block">
+          <span>Product</span>
+        </div>
+        <div className="header-block">
+          <span>Description</span>
+        </div>
+        <div className="header-block">
+          <span>Quantity</span>
+        </div>
+        <div className="header-block">
+          <span>Price</span>
+        </div>
+        <div className="header-block">
+          <span>Remove</span>
+        </div>
+      </div>
+      {cartItems.map((cartItem) => (
+        <CheckoutItem cartItem={cartItem} />
+      ))}
+
+      <div className="total">
+        <span>TOTAL: ${totalPrice}</span>
+      </div>
+    </div>
+  );
+};
+
+export default Checkout;
