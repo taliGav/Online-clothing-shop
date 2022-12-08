0bc14515f8d7380ea3793e84d3a19833921ea7bd
Tali <taligavrielov@gmail.com>
2022-12-09 00:21:08 +0200

    update app routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { Routes, Route } from "react-router-dom";
 import Navigation from "./routes/navigation/Navigation";
+import Home from "./routes/Home";
 import Shop from "./routes/shop/Shop";
 import Authentication from "./routes/authentication/Authentication";
-import Home from "./routes/Home";
+import Dashboard from "./routes/dashboard/Dashboard";
+import Checkout from "./routes/checkout/Checkout";
 
 
 const App = () => {
@@ -13,6 +15,8 @@ const App = () => {
         <Route path="shop" element={<Shop />} />
         {/* <Route path="contact" element={<Contact />} /> */}
         <Route path="auth" element={<Authentication />} />
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="checkout" element={<Checkout />} />
       </Route>
     </Routes>
   );
