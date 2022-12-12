37af732fd7b613a76fd6b98ad35a26eb1cdfa654
Tali <taligavrielov@gmail.com>
2022-12-12 10:16:00 +0200

    convert products provider to categories provider

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 import { UserProvider } from "./contexts/user-context";
-import { ProductsProvider } from "./contexts/products-context";
+import { CategoriesProvider } from "./contexts/categories-context";
 import { CartStatusProvider } from "./contexts/cart-context";
 
 import "./index.scss";
@@ -23,11 +23,11 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <ProductsProvider>
+        <CategoriesProvider>
           <CartStatusProvider> 
           <App />
           </CartStatusProvider>
-        </ProductsProvider>
+        </CategoriesProvider>
       </UserProvider>
       {/* <RouterProvider router={router} /> */}
     </BrowserRouter>
