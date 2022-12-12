de0d04e47a805b7ce7006846aa4c9b6cde874d52
Tali <taligavrielov@gmail.com>
2022-12-12 12:12:35 +0200

    add link to title

--- src/components/category-preview/CategoryPreview.js
+++ src/components/category-preview/CategoryPreview.js
@@ -1,10 +1,14 @@
+import { Link } from "react-router-dom";
+
 import ProductCard from "../product-card/ProductCard";
+
 import "./category-preview.styles.scss";
+
 
 const CategoryPreview = ({ title, products }) => (
   <div className="category-preview-container">
     <h2>
-      <span className="title">{title.toUpperCase()}</span>
+      <Link className="title" to={title}>{title.toUpperCase()}</Link>
     </h2>
     <div className="preview">
       {products
