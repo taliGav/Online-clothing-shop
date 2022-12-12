a826df7ef7068ba8949019f2f9ddf4c1cdefc23b
Tali <taligavrielov@gmail.com>
2022-12-12 12:11:07 +0200

    rename categories to directory

---
+++ src/components/directory/Directory.js
@@ -0,0 +1,14 @@
+import DirectoryItem from "./../directory-item/DirectoryItem";
+import "./directory.styles.scss";
+
+function Directory({ categories }) {
+  return (
+    <div className="directory-container">
+      {categories.map((category) => (
+        <DirectoryItem key={category.id} category={category} />
+      ))}
+    </div>
+  );
+}
+
+export default Directory;
