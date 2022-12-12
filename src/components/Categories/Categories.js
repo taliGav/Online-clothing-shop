673af3468c76e699827fef46c279394101f0e865
Tali <taligavrielov@gmail.com>
2022-12-12 10:16:44 +0200

    add categories to component

--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -1,16 +1,13 @@
 import CategoryItem from "../category-item/CategoryItem";
 import "./categories.styles.scss";
 
-function Categories({categories}) {
-
+function Categories({ categories }) {
   return (
     <div className="categories">
       <h2>Categories</h2>
       <div className="categories__container">
         {categories.map((category) => (
-          <CategoryItem 
-		  key={category.id}
-		  category={category} />
+          <CategoryItem key={category.id} category={category} />
         ))}
       </div>
     </div>
