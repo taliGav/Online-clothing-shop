755d50d35fcdc36ee23d51d3c341d62f953dff00
Tali <taligavrielov@gmail.com>
2022-12-12 12:13:07 +0200

    add button props

--- src/components/product-card/ProductCard.js
+++ src/components/product-card/ProductCard.js
@@ -1,27 +1,30 @@
-import "./product-card.styles.scss";
 import { useContext } from "react";
+
 import { CartContext } from "./../../contexts/cart-context";
+
 import Button from "./../button/Button";
+
+import "./product-card.styles.scss";
 
 const ProductCard = ({ product }) => {
   const { name, price, imageUrl } = product;
   const { addItemToCart } = useContext(CartContext);
 
-  const handleAddToCart = () => addItemToCart(product)
+  const handleAddToCart = () => addItemToCart(product);
 
   return (
     <div className="product-card-container">
-      {/* <div className="product-card-image"> */}
-        <img
-          src={imageUrl ? imageUrl : "https://via.placeholder.com/150"}
-          alt={name}
-        />
-      {/* </div> */}
+      <img
+        src={imageUrl ? imageUrl : "https://via.placeholder.com/150"}
+        alt={name}
+      />
       <div className="product-card-info">
         <span className="name">{name}</span>
         <span className="price">${price}</span>
       </div>
-      <Button onClick={handleAddToCart}>Add to cart</Button>
+      <Button buttonType="inverted" onClick={handleAddToCart}>
+        Add to cart
+      </Button>
     </div>
   );
 };
