d42385f1d2219954e4029c5c93c50cbd87907fb8
Tali <taligavrielov@gmail.com>
2022-12-13 17:35:25 +0200

    update

--- src/components/category-preview/CategoryPreview.js
+++ src/components/category-preview/CategoryPreview.js
@@ -1,8 +1,10 @@
-import { Link } from "react-router-dom";
-
 import ProductCard from "../product-card/ProductCard";
 
-import {CategoryPreviewContainer, Title, Preview} from "./category-preview.styles";
+import {
+  CategoryPreviewContainer,
+  Title,
+  Preview,
+} from "./category-preview.styles";
 
 
 const CategoryPreview = ({ title, products }) => (
