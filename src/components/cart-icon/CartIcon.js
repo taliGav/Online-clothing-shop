d028178cd162d33c54229b4603731e00ae046509
Tali <taligavrielov@gmail.com>
2022-12-13 17:23:17 +0200

    update CartIcon styles

--- src/components/cart-icon/CartIcon.js
+++ src/components/cart-icon/CartIcon.js
@@ -1,16 +1,17 @@
-import { ReactComponent as ShoppingIcon } from "./../../assets/shopping-bag.svg";
 import { useContext } from "react";
 import { CartContext } from "./../../contexts/cart-context";
-import "./cart-icon.styles.scss";
+
+import {CartIconContainer, ShoppingIcon, ItemCount
+} from "./cart-icon.styles.js";
 
 const CartIcon = ({onClick}) => {
   const {cartItemsCount} = useContext(CartContext);
 
   return (
-    <div onClick={onClick} className="cart-icon-container">
+    <CartIconContainer onClick={onClick}>
       <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">{cartItemsCount}</span>
-    </div>
+      <ItemCount>{cartItemsCount}</ItemCount>
+    </CartIconContainer>
   );
 };
 
