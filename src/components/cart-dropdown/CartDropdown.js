d944471cb39a10d0a14cfe33badb77afdf6d1709
Tali <taligavrielov@gmail.com>
2022-12-13 17:07:09 +0200

    update CartDropdown cmp styles

--- src/components/cart-dropdown/CartDropdown.js
+++ src/components/cart-dropdown/CartDropdown.js
@@ -1,4 +1,3 @@
-import "./cart-dropdown.styles.scss";
 import { useContext } from "react";
 import { CartContext } from "./../../contexts/cart-context";
 import { Link } from "react-router-dom";
@@ -6,22 +5,33 @@ import { Link } from "react-router-dom";
 import CartItem from "./../cart-item/CartItem";
 import Button from "./../button/Button";
 
-const CartDropdown = () => {
-  const { cartItems , toggleCartDropdown} = useContext(CartContext);
+import {
+  CartDropdownContainer,
+  CartItems,
+  EmptyMessage,
+} from "./cart-dropdown.styles";
 
-  console.log("cartItems on cartdropdown", cartItems);
+const CartDropdown = () => {
+  const { cartItems, toggleCartDropdown } = useContext(CartContext);
+
+  // console.log("cartItems on cartdropdown", cartItems);
 
   return (
-    <div className="cart-dropdown-container">
-      <div className="cart-items">
-        {cartItems.map((item) => (
-          <CartItem key={item.id} cartItem={item} />
-        ))}
-      </div>
+    <CartDropdownContainer>
+      <CartItems>
+        {cartItems.length ? (
+          cartItems.map((item) => <CartItem key={item.id} cartItem={item} />)
+        ) : (
+          <EmptyMessage>Your cart is empty</EmptyMessage>
+        )}
+      </CartItems>
+      {/* <Link to="/checkout" onClick={toggleCartDropdown}>
+       GO TO CHECKOUT
+      </Link> */}
       <Link to="/checkout">
         <Button onClick={toggleCartDropdown}>GO TO CHECKOUT</Button>
       </Link>
-    </div>
+    </CartDropdownContainer>
   );
 };
 
