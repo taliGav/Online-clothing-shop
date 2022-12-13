d76d84f6e7209d0cd2f86554af33670fcfe2392f
Tali <taligavrielov@gmail.com>
2022-12-13 15:40:58 +0200

    update Buton cmp styles to styled components using classes

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -1,16 +1,36 @@
-import "./button.styles.scss"
+import {
+  BaseButton,
+  GoogleSignInButton,
+  InvertedButton,
+} from "./button.styles";
 
-const BUTTON_TYPE_CLASSES = {
+export const BUTTON_TYPE_CLASSES = {
+  base: "base",
   google: "google-sign-in",
   inverted: "inverted",
 };
 
+const getButtonByType = (buttonType) => {
+  switch (buttonType) {
+    case BUTTON_TYPE_CLASSES.google:
+      return GoogleSignInButton;
+    case BUTTON_TYPE_CLASSES.inverted:
+      return InvertedButton;
+    default:
+      return BaseButton;
+  }
+};
+
+// const getButtonByType = (buttonType = BUTTON_TYPE_CLASSES.base) =>
+//   ({
+//     [BUTTON_TYPE_CLASSES.google]: GoogleSignInButton,
+//     [BUTTON_TYPE_CLASSES.inverted]: InvertedButton,
+//     [BUTTON_TYPE_CLASSES.base]: BaseButton,
+//   }[buttonType]);
+
 const Button = ({ children, buttonType, ...otherProps }) => {
-  return (
-    <button className={`button-container ${BUTTON_TYPE_CLASSES[buttonType]}`} {...otherProps}>
-      {children}
-    </button>
-  );
+  const CustomButton = getButtonByType(buttonType);
+  return <CustomButton {...otherProps}> {children} </CustomButton>;
 };
 
 export default Button;
