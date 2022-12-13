6fb9df17fa5465da4c2840e20e100ff97bd69c07
Tali <taligavrielov@gmail.com>
2022-12-13 17:01:47 +0200

    move categories to directory from home cmp

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,39 +1,15 @@
+import { Outlet } from 'react-router-dom';
+
 import Directory from "../components/directory/Directory";
 
-function Home() {
-  const categories = [
-    {
-      id: 1,
-      title: "HATS",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 2,
-      title: "JACKETS",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "SNEAKERS",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: 4,
-      title: "WOMEN",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "MEN",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-  ];
-
+const Home = () => {
   return (
-    <div className="Home">
-      <Directory categories={categories} />
+    <div>
+      <Directory />
+      <Outlet />
     </div>
   );
-}
+};
+
 
 export default Home;
