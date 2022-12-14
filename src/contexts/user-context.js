56abc21e9c14f3e6382383ae630e9a522bb878fd
Tali <taligavrielov@gmail.com>
2022-12-14 11:43:45 +0200

    delete comments

--- src/contexts/user-context.js
+++ src/contexts/user-context.js
@@ -1,8 +1,16 @@
-import { createContext, useEffect, useReducer } from "react";
+import { 
+  createContext, 
+  useEffect, 
+  useReducer 
+} from "react";
+
 import {
   onAuthStateChangedListener,
   createUserProfileDocument,
 } from "./../utils/firebase/firebase-utils";
+
+
+
 
 export const UserContext = createContext({
   currentUser: null,
@@ -14,8 +22,6 @@ export const USER_ACTIONS_TYPES = {
 };
 
 const userReducer = (state, action) => {
-  console.log("dispatched action: ", action);
-
   const { type, payload } = action;
   switch (type) {
     case "SET_CURRENT_USER":
@@ -37,7 +43,6 @@ export const UserProvider = ({ children }) => {
   const [state, dispatch] = useReducer(userReducer, INITIAL_STATE);
 
   const { currentUser } = state;
-  console.log("currentUser", currentUser);
 
   const setCurrentUser = (user) => {
     dispatch({ type: USER_ACTIONS_TYPES.SET_CURRENT_USER, payload: user });
@@ -57,18 +62,3 @@ export const UserProvider = ({ children }) => {
 
   return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
 };
-
-/*
-const userReducer = (state, action) => {
-  switch (action.type) {
-    case "SET_CURRENT_USER":
-      return {
-        ...state,
-        currentUser: action.payload,
-      };
-    default:
-      return state;
-  }
-
-
-*/
